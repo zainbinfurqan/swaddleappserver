f56856b33ca682596e80aa0bd2d2a9af1b46c6fd
zainbinfurqan <zain.ahmed199524@gmail.com>
2020-02-13 00:28:56 +0500

    updated

--- graphql/resolvers/authenticationResolver/loginResolver.js
+++ graphql/resolvers/authenticationResolver/loginResolver.js
@@ -18,15 +18,22 @@ let User = require('../../../models/user/userSchema'),
 //@parmas required (email, password)
 
 exports.login = async ({ email, password }) => {
-
+    console.log(email, password)
     if (email && password) {
         let login = await genericFunctions._baseFetch(User, { query: { email } }, 'FindOne')
-        const isEqual = await bcrypt.compare(password, login.data.password);
-        if (isEqual) {
-            const token = jwt.sign({ userId: login._id, email: login.email }, JWT_SECRET, { expiresIn: '1h' });
-            return { status: true, statusCode: 200, message: "Login Successfully", userId: login._id, email: login.email, token }
+        console.log(login)
+        if (login.data) {
+            const isEqual = await bcrypt.compare(password, login.data.password);
+            console.log(isEqual)
+            if (isEqual) {
+                const token = jwt.sign({ userId: login._id, email: login.email }, JWT_SECRET, { expiresIn: '1h' });
+                return { status: true, statusCode: 200, message: "Login Successfully", userId: login._id, email: login.email, token }
+            }
+            return { status: false, statusCode: 203, message: "invalid email or password", userId: '', email: '', token: '' }
         }
-        return { status: false, statusCode: 203, message: "invalid email or password", userId: '', email: '', token: '' }
+        else {
+            return { status: false, statusCode: 203, message: "invalid email or password", userId: '', email: '', token: '' }
+        }
     } else {
         return { status: false, statusCode: 203, message: "please fill all the fields", userId: '', email: '', token: '' }
     }
